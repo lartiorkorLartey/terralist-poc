740a7345bf978cfa378e2398f6c7b1d2aeb6ddf6
lartiorkorLartey <tiorkordotdev@gmail.com>
2025-10-22 14:44:12 +0000

    replace with sample vpc module

--- variables.tf
+++ variables.tf
@@ -1,4 +1,17 @@
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
+# variable "bucket_name" {
+#   description = "The name of the S3 bucket"
+#   type        = string
+# }
+
+
+variable "vpc_id" {
+  description = "Existing VPC to use (specify this, if you don't want to create new VPC)"
   type        = string
+  default     = ""
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
+  type        = string
+  default     = "0.0.0.0/0"
 }
